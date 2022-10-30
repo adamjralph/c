858434ee0e715c4567807429c420606e89d5559b
Adam Ralph <adamralph@posteo.net>
2022-10-30 22:10:47 +1100

    updates

---
+++ 09-logical.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+  float temp = 25;
+  bool sunny = true;
+
+  if (temp >= 0 && temp <= 30 && sunny) {
+    printf("\nThe weather is good!");
+  }
+  else {
+    printf("\nThe weather is bad!");
+  }
+}
