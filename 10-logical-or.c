31b6a36dc1f21ca3fbe633a8f39f9cc6bdbaa383
Adam Ralph <adamralph@posteo.net>
2022-10-31 08:58:43 +1100

    updates

--- 10-logical-or.c
+++ 10-logical-or.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
+int main() 
 
-  float temp = 25;
+{
+   
+  float temp = -1000;
   bool sunny = true;
 
-  if (temp >= 0 && temp <= 30 && sunny) {
-    printf("\nThe weather is good!");
+  if (temp <= 0 || temp >= 30) {
+    printf("\nThe weather is bad!");
   }
   else {
     printf("\nThe weather is bad!");
   }
   return 0;
+
 }
+
